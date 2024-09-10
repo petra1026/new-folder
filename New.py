cef4715ba2e995a4ecf3a12bb1a2a6e34fde680c
petra1026 <149487838+petra1026@users.noreply.github.com>
2024-09-10 11:17:24 +0300

    Create New.py

---
+++ New.py
@@ -0,0 +1,14 @@
+print("Hello!")
+class Cilvēks: 
+    def__init__(self, name, age, sex):
+    self.name=vārds
+    self.age=vecums
+    self.sex=dzimums
+
+persona=Cilvēks("Anna", "18", "Female")
+print(persona.name)
+persona.name="Anna"
+print(persona.age)
+persona.age= "18"
+print(persona.sex)
+persona.sex= "Female"
